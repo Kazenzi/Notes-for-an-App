17bb2b7741e0a7b94d895f0d4466a81d8774d7e3
Kazenzi <jacintakazenzi99@gmail.com>
2023-12-30 17:17:39 +0300

    mm

--- app/src/main/java/com/example/mynoteapp/room/DatabaseConfig.kt
+++ app/src/main/java/com/example/mynoteapp/room/DatabaseConfig.kt
@@ -1,2 +1,19 @@
 package com.example.mynoteapp.room
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.Room.databaseBuilder
+import androidx.room.RoomDatabase
+
+object DatabaseConfig {
+    fun getInstance(context: Context):AppDatabase{
+        var db= databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "note_db"
+        ).build()
+        return db
+    }
+}
+
